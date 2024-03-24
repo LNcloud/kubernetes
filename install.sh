d2298448ed0f36dc5ab2ca365609462f37f595bc
LNcloud <161946390+LNcloud@users.noreply.github.com>
2024-03-24 19:10:28 +0530

    commands to install kubernetes cluster

--- install.sh
+++ install.sh
@@ -1,20 +1,38 @@
 #! /bin/bash
 
+# run the following commands on all the nodes
 sudo apt-get update
-# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt install docker.io -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-
-# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
-# sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
 sudo systemctl enable --now kubelet
 
+#..............................................................................................end.....................................
+
+
+
+
+
+#On Master:
+#sudo kubeadm init --apiserver-advertise-address=privateipofmaster
+
+#Note: You need to replace “private_ip_of_master” with the actual private ip of your kubernetes master.
+
+#Paste the Token on Slave
+
+#On Master
+
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Installing Calico
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml -O
+
+#kubectl apply -f calico.yaml
+
+#kubectl get nodes
